4524bec8781fce0865aa894f14d563cf20180a40
Volodymyr <salashchenkovladymyr@gmail.com>
2025-12-28 15:55:44 +0200

    ready task1 - task4

--- src/task-1.ts
+++ src/task-1.ts
@@ -1,2 +1,7 @@
-const message: string = "TypeScript працює!";
-console.log(message);
+const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
