379466b9fee3e4ac1e6c044a6c81ecf015a14fc6
Volodymyr <salashchenkovladymyr@gmail.com>
2025-12-28 18:38:23 +0200

    added axios + task5-task8 is done

--- src/task-7.ts
+++ src/task-7.ts
@@ -1,1 +1,11 @@
+type T = string;
 
+function getMessage(): Promise<T> {
+  return new Promise<T>((resolve) => {
+    setTimeout(() => {
+      resolve("Hello from TS");
+    }, 1000);
+  });
+}
+
+getMessage().then((result) => console.log(result));
