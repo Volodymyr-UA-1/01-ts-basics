a0f34a06c6aff6dabcc5cec7fbb2495eff62140a
Volodymyr <salashchenkovladymyr@gmail.com>
2025-12-29 21:36:11 +0200

    correct mistake task-8.ts

--- src/task-8.ts
+++ src/task-8.ts
@@ -5,12 +5,12 @@ interface Post {
   title: string;
   body: string;
 }
-async function fetchPosts() {
+const fetchPosts = async (): Promise<Post[]> => {
   const response = await axios.get<Post[]>(
     "https://jsonplaceholder.typicode.com/posts"
   );
   return response.data;
-}
+};
 
 fetchPosts().then((posts) => {
   console.log(posts[0].title);
